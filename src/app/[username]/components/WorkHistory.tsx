c52b90edc3607d3ba702d8b6087806d7c6ca28f3
ethan <epollack31@gmail.com>
2024-05-08 14:06:20 -0400

    created Timeline component and implemented WorkHistory using it

--- src/app/[username]/components/WorkHistory.tsx
+++ src/app/[username]/components/WorkHistory.tsx
@@ -1,3 +1,4 @@
+import Timeline from "@/components/Timeline";
 import { WorkExperience } from "@/lib/testData";
 
 const options = {
@@ -23,32 +24,27 @@ export default function WorkHistory({
   return (
     <div className="my-8 max-w-prose">
       <h3 className="text-3xl mb-4">Work History</h3>
-      <div className="flex flex-col">
-        {workHistory.map((work) => (
-          <div
-            key={work.company}
-            className="relative  border-l-2 px-4 [&:not(:last-child)]:pb-8 hover:scale-110 origin-left transition-transform"
-          >
-            <span
-              className={`absolute top-0 left-0 w-4 h-4 rounded-full border border-red-300 ${
-                !work.endDate ? "bg-red-300" : "bg-red-50"
-              } translate-x-[calc(-50%-1px)] translate-y-1/2`}
-            />
-            <h4 className="text-xl font-semibold">{work.title}</h4>
-            <div className="flex flex-wrap justify-between">
-              <p className=""> {work.company}</p>
-              <p className="text-sm ">
-                ({work.startDate.toLocaleDateString("en-US", options)} -{" "}
-                {work.endDate
-                  ? work.endDate.toLocaleDateString("en-US", options)
-                  : "Present"}
-                )
-              </p>
-            </div>
-            <p>{work.description}</p>
-          </div>
-        ))}
-      </div>
+      <Timeline
+        items={workHistory.map((work) => ({
+          node: (
+            <>
+              <h4 className="text-xl font-semibold">{work.title}</h4>
+              <div className="flex flex-wrap justify-between">
+                <p className=""> {work.company}</p>
+                <p className="text-sm ">
+                  ({work.startDate.toLocaleDateString("en-US", options)} -{" "}
+                  {work.endDate
+                    ? work.endDate.toLocaleDateString("en-US", options)
+                    : "Present"}
+                  )
+                </p>
+              </div>
+              <p>{work.description}</p>
+            </>
+          ),
+          active: !work.endDate,
+        }))}
+      />
     </div>
   );
 }
---
+++ src/components/Timeline.tsx
@@ -0,0 +1,26 @@
+export default function Timeline({
+  items,
+}: {
+  items: {
+    node: React.ReactNode;
+    active: boolean;
+  }[];
+}) {
+  return (
+    <div className="flex flex-col">
+      {items.map((item, index) => (
+        <div
+          key={index}
+          className="relative  border-l-2 px-4 [&:not(:last-child)]:pb-8 hover:scale-110 origin-left transition-transform"
+        >
+          <span
+            className={`absolute top-0 left-0 w-4 h-4 rounded-full border border-red-300 ${
+              item.active ? "bg-red-300" : "bg-red-50"
+            } translate-x-[calc(-50%-1px)] translate-y-1/2`}
+          />
+          {item.node}
+        </div>
+      ))}
+    </div>
+  );
+}
