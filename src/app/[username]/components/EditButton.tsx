095d58917c833b2ec9e964fa1833217230c35bd8
ethan <epollack31@gmail.com>
2024-05-08 22:49:33 -0400

    fixed EditButton redirect

--- src/app/[username]/components/EditButton.tsx
+++ src/app/[username]/components/EditButton.tsx
@@ -7,7 +7,7 @@ import { createPortal } from "react-dom";
 export default function EditButton() {
   return (
     <Link
-      href="/edit"
+      href="/profile"
       className="fixed bottom-4 right-4 rounded-full p-2 bg-blue-400/20 backdrop-blur-sm shadow-lg hover:scale-110 active:scale-95 transition-transform z-50 "
     >
       <Pencil className="w-6 h-6 cursor-pointer" />
