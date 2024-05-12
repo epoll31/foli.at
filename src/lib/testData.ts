7a303b367fa6dc0d33fd5f662c0426d4dd9d8358
ethan <epollack31@gmail.com>
2024-05-12 10:23:09 -0400

    updated testData's types

--- src/lib/testData.ts
+++ src/lib/testData.ts
@@ -1,155 +1,87 @@
-export interface Link {
-  href: string;
-  type: "github" | "linkedin" | "twitter" | "portfolio" | "other";
-}
-export interface WorkExperience {
-  title: string;
-  company: string;
-  startDate: Date;
-  endDate?: Date;
-  description: string;
-}
+import { PortfolioGroup } from "./types";
 
-export interface EducationExperience {
-  degree: string;
-  school: string;
-  startDate: Date;
-  endDate?: Date;
-  description: string;
-}
-
-export interface User {
-  username: string;
-  fullName: string;
-  email?: string;
-  phone?: string;
-  links: Link[];
-  bio: string;
-  title: string;
-  workHistory: WorkExperience[];
-  educationHistory: EducationExperience[];
-}
-
-export const testData: User[] = [
+export const testData: PortfolioGroup[] = [
   {
-    username: "epoll31",
-    fullName: "Ethan Pollack",
-    email: "epollack31@gmail.com",
-    phone: "(818)-398-8996",
+    portfolio: {
+      id: 0,
+      tag: "epoll31",
+      full_name: "Ethan Pollack",
+      bio: "I am a software engineer with a passion for learning and teaching. I love to build things and help others do the same.",
+      title: "Software Engineer",
+    },
     links: [
       {
+        id: 0,
         href: "https://github.com/epoll31",
         type: "github",
       },
       {
+        id: 1,
         href: "https://www.linkedin.com/in/ethanpollack/",
         type: "linkedin",
       },
       {
+        id: 2,
         href: "https://twitter.com/epoll31",
         type: "twitter",
       },
       {
+        id: 3,
         href: "https://epoll31.github.io",
         type: "portfolio",
       },
       {
+        id: 4,
         href: "https://google.com",
         type: "other",
       },
     ],
-    bio: "I am a software engineer with a passion for learning and teaching. I love to build things and help others do the same.",
-    title: "Software Engineer",
-    workHistory: [
+    workEntries: [
       {
+        id: 0,
         title: "Software Engineer",
         company: "Google",
-        startDate: new Date("2018-01-01"),
-        endDate: new Date("2019-12-31"),
+        start_date: new Date("2018-01-01"),
+        end_date: new Date("2019-12-31"),
         description:
           "I worked on the Google Search team. I built the search bar. It was fun.",
       },
       {
+        id: 1,
         title: "Software Engineer",
         company: "Facebook",
-        startDate: new Date("2020-01-01"),
-        endDate: new Date("2021-12-31"),
+        start_date: new Date("2020-01-01"),
+        end_date: new Date("2021-12-31"),
         description:
           "I worked on the Facebook Ads team. I built the ads. It was fun. I also built the search bar. It was fun.",
       },
       {
+        id: 2,
         title: "Software Engineer",
         company: "Amazon",
-        startDate: new Date("2022-01-01"),
-        endDate: undefined,
+        start_date: new Date("2022-01-01"),
+        end_date: null,
         description:
           "I worked on the Amazon Web Services team. I built the web services. It was fun. I also built the search bar. It was fun. I also built the ads. It was fun.",
       },
     ],
-    educationHistory: [
+    educationEntries: [
       {
+        id: 0,
         degree: "Bachelor of Science",
+        major: "Computer Science",
         school: "Worcester Polytechnic Institute",
-        startDate: new Date(2020, 8),
-        endDate: new Date(2024, 5),
+        start_date: new Date(2020, 8),
+        end_date: new Date(2024, 5),
         description: "I studied Computer Science and Engineering.",
       },
       {
+        id: 1,
         degree: "High School Diploma",
+        major: null,
         school: "Calabasas High School",
-        startDate: new Date(2016, 8),
-        endDate: new Date(2020, 5),
-        description: "I studied Computer Science and Engineering.",
-      },
-    ],
-  },
-  {
-    username: "johndoe",
-    fullName: "John Doe",
-    email: "jd@gmail.com",
-    phone: "(555)-555-5555",
-    links: [],
-    bio: "I am a software engineer with a passion for learning and teaching. I love to build things and help others do the same.",
-    title: "Software Engineer",
-    workHistory: [
-      {
-        title: "Software Engineer",
-        company: "Google",
-        startDate: new Date("2018-01-01"),
-        endDate: new Date("2019-12-31"),
-        description:
-          "I worked on the Google Search team. I built the search bar. It was fun.",
-      },
-      {
-        title: "Software Engineer",
-        company: "Facebook",
-        startDate: new Date("2020-01-01"),
-        endDate: new Date("2021-12-31"),
-        description:
-          "I worked on the Facebook Ads team. I built the ads. It was fun. I also built the search bar. It was fun.",
-      },
-      {
-        title: "Software Engineer",
-        company: "Amazon",
-        startDate: new Date("2022-01-01"),
-        endDate: undefined,
-        description:
-          "I worked on the Amazon Web Services team. I built the web services. It was fun. I also built the search bar. It was fun. I also built the ads. It was fun.",
-      },
-    ],
-    educationHistory: [
-      {
-        degree: "Bachelor of Science",
-        school: "Worcester Polytechnic Institute",
-        startDate: new Date(2020, 8),
-        endDate: new Date(2024, 5),
-        description: "I studied Computer Science and Engineering.",
-      },
-      {
-        degree: "High School Diploma",
-        school: "Calabasas High School",
-        startDate: new Date(2016, 8),
-        endDate: new Date(2020, 5),
+        start_date: new Date(2016, 8),
+        end_date: new Date(2020, 5),
         description: "I studied Computer Science and Engineering.",
       },
     ],
