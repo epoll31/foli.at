f621513095c30469578848691c7cad9c8815597d
ethan <epollack31@gmail.com>
2024-05-15 10:22:13 -0700

    added prisma and postgres to store users in database

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,12 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [GitHub, Google],
 });
