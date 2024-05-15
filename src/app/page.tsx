da45f33227d0272965f254db27d1b21cd651933b
ethan <epollack31@gmail.com>
2024-05-15 13:50:16 -0700

    removed form from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,22 +7,6 @@ export default async function Home() {
   return (
     <>
       <h2 className="text-4xl text-center">Build Your Portfolio Now!</h2>
-      {session ? (
-        <>
-          <form
-            action={async () => {
-              "use server";
-              await signOut();
-            }}
-          >
-            <button type="submit" className="bg-red-200 rounded-lg px-3 py-1">
-              Sign Out
-            </button>
-          </form>
-        </>
-      ) : (
-        <SignInForm />
-      )}
     </>
   );
 }
