164cc41ba07216831a690c41fb6aef2ddd1f43fd
ethan <epollack31@gmail.com>
2024-05-14 22:37:44 -0700

    added auth.js with ouath2 support with google and github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,55 @@
-import {
-  Accordion,
-  AccordionContent,
-  AccordionTrigger,
-} from "@/components/ui/Accordion";
+import { auth, signIn, signOut } from "@/auth";
+import Github from "@/components/icons/github";
+import Google from "@/components/icons/google";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
     <>
       <h2 className="text-4xl text-center">Build Your Portfolio Now!</h2>
-      <Accordion className="w-full">
-        <AccordionTrigger className=" ">Work</AccordionTrigger>
-        <AccordionContent className="bg-blue-300">
-          <p>Work content goes here</p>
-          <p>Work content goes here</p>
-          <p>Work content goes here</p>
-          <p>Work content goes here</p>
-        </AccordionContent>
-      </Accordion>
+      {session ? (
+        <form
+          action={async () => {
+            "use server";
+            await signOut();
+          }}
+        >
+          <button type="submit" className="bg-red-200 rounded-lg px-3 py-1">
+            Sign Out
+          </button>
+        </form>
+      ) : (
+        <div className="flex flex-col gap-3">
+          <form
+            action={async () => {
+              "use server";
+              await signIn("google");
+            }}
+          >
+            <button
+              type="submit"
+              className="border-2 border-blue-200 bg-transparent hover:bg-blue-200 active:bg-blue-300 rounded-lg px-3 py-2 text-xl w-full flex items-center justify-between gap-3 transition-colors duration-500"
+            >
+              <Google className="text-2xl" /> Sign in with Google
+            </button>
+          </form>
+          <form
+            action={async () => {
+              "use server";
+              await signIn("github");
+            }}
+          >
+            <button
+              type="submit"
+              className="border-2 border-blue-200 bg-transparent hover:bg-blue-200 active:bg-blue-300 rounded-lg px-3 py-2 w-full  text-xl flex items-center justify-between gap-3 transition-colors duration-500"
+            >
+              <Github className="text-2xl" />
+              Sign in with GitHub
+            </button>
+          </form>
+        </div>
+      )}
     </>
   );
 }
