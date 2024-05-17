fcf4b54eb402d84d1780cd45e3bb6d5754eb58b0
ethan <epollack31@gmail.com>
2024-05-17 09:10:38 -0700

    added custom icons to drowdowna dn textarea

--- src/components/ui/DropDown.tsx
+++ src/components/ui/DropDown.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/utils/cn";
 import GlowContainer from "../GlowContainer";
 import React from "react";
+import ChevronUp from "../icons/chevron-up";
 
 export interface Option {
   value: string;
@@ -23,7 +24,7 @@ const DropDown = React.forwardRef(
     return (
       <GlowContainer
         padding="2px"
-        className="rounded-[0.5rem] w-full  bg-zinc-700/60 backdrop-blur-sm"
+        className="relative rounded-[0.5rem] w-full  bg-zinc-700/60 backdrop-blur-sm "
         glowColor={glowColor}
       >
         <select
@@ -31,6 +32,9 @@ const DropDown = React.forwardRef(
             "w-full px-2 py-1 rounded-[calc(0.5rem-2px)] focus-visible:outline-none border-2 border-transparent focus-visible:border-blue-300/50 bg-zinc-800/50 text-white/60 z-20",
             className
           )}
+          style={{
+            appearance: "none",
+          }}
           ref={ref}
           {...props}
         >
@@ -40,6 +44,7 @@ const DropDown = React.forwardRef(
             </option>
           ))}
         </select>
+        <ChevronUp className="absolute w-5 h-5 text-zinc-500 right-1 top-1/2 -translate-y-1/2 py-auto pointer-events-none -scale-y-100" />
       </GlowContainer>
     );
   }
--- src/components/ui/TextArea.tsx
+++ src/components/ui/TextArea.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/utils/cn";
 import GlowContainer from "../GlowContainer";
 import React from "react";
+import GridDots from "../icons/grid-dots";
 
 const TextArea = React.forwardRef(
   (
@@ -16,17 +17,22 @@ const TextArea = React.forwardRef(
     return (
       <GlowContainer
         padding="2px"
-        className="rounded-[0.5rem] bg-zinc-700/60 backdrop-blur-sm"
+        className="relative rounded-[0.5rem] bg-zinc-700/60 backdrop-blur-sm"
         glowColor={glowColor}
       >
         <textarea
           className={cn(
             className,
-            "w-full px-2 py-1 rounded-[calc(0.5rem-2px)] focus-visible:outline-none border-2 border-transparent focus-visible:border-blue-300/50 bg-zinc-800/50 text-white/60 z-20 min-h-10 max-h-96 align-top"
+            " w-full px-2 py-1 rounded-[calc(0.5rem-2px)] focus-visible:outline-none border-2 border-transparent focus-visible:border-blue-300/50 bg-zinc-800/50 text-white/60 z-20 min-h-10 max-h-96 align-top [&::-webkit-resizer]:hidden"
           )}
+          style={{
+            scrollbarColor: "var(--color-zinc-500) transparent",
+          }}
           ref={ref}
           {...props}
-        />
+        ></textarea>
+        {/* <span className="absolute w-4 h-4 bg-red-300 right-0 bottom-0 pointer-events-none" /> */}
+        <GridDots className="absolute w-4 h-4 text-zinc-500 right-0.5 bottom-0.5 pointer-events-none" />
       </GlowContainer>
     );
   }
