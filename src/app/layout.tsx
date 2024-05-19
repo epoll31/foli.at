726b43f6f88cb530983be604e6fb802634d77238
ethan <epollack31@gmail.com>
2024-05-19 15:50:28 -0700

    added theme transition to root document

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default async function RootLayout({
   const session = await auth();
 
   return (
-    <html lang="en">
+    <html lang="en" className=" transition-light-dark">
       <body
         className={cn(
           inter.className,
