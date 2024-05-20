35b12ea7c77405a2061d49e810ec979e70d0fc59
ethan <epollack31@gmail.com>
2024-05-19 20:33:54 -0700

    added error messages to signin page / fixed theme transition on sign in page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,13 +1,17 @@
 import BackgroundGrid from "@/components/BackgroundGrid";
 import SignInForm from "@/components/forms/SignInForm";
+import { cookies } from "next/headers";
 
 export default function SignInPage() {
+  const cookieStore = cookies();
+  const error = cookieStore.get("error");
+
   return (
     <>
       <BackgroundGrid size={30} fade fadeFrom="30%" fadeTo="100%" />
-      <div className="rounded-xl overflow-hidden bg-theme-bg-secondary drop-shadow-lg border border-theme-border-primary">
-        <div className="sm:px-16  py-6 border-b border-b-theme-border-primary">
-          <h2 className="text-theme-text-primary text-3xl md:text-4xl text-center text-nowrap">
+      <div className="rounded-xl overflow-hidden bg-theme-bg-secondary drop-shadow-lg border border-theme-border-primary transition-light-dark">
+        <div className="sm:px-16  py-6 border-b border-b-theme-border-primary  transition-light-dark">
+          <h2 className="text-theme-text-primary text-3xl md:text-4xl text-center text-nowrap transition-light-dark">
             Sign In
           </h2>
         </div>
@@ -15,6 +19,11 @@ export default function SignInPage() {
           <SignInForm />
         </div>
       </div>
+      {error && (
+        <div className=" bg-theme-error rounded-lg px-5 py-2 text-theme-bg-primary m-3 max-w-96 text-center text-pretty transition-colors">
+          <p>{error?.value}</p>
+        </div>
+      )}
     </>
   );
 }
